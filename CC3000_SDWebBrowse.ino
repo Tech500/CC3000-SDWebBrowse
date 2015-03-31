4fc8b4d945bbcf6944bed8c7a7686766133d8dd9
Tech500 <wmmike@gmail.com>
2015-03-31 10:34:30 -0400

    Orginial version, "Adafruit/SDWebBrowse.ino"; modified for use with "Adafruit CC3000 Shield" by "Tech500" particpant on "Adafruit Forum."
    Signed-off-by: Tech500 <wmmike@gmail.com>

---
+++ CC3000_SDWebBrowse.ino
@@ -0,0 +1,383 @@
+/*
+ * USERS OF ARDUINO 0023 AND EARLIER: use the 'SDWebBrowse.pde' sketch...
+ * 'SDWebBrowse.ino' can be ignored.
+ * USERS OF ARDUINO 1.0 AND LATER: **DELETE** the 'SDWebBrowse.pde' sketch 
+ * and use ONLY the 'SDWebBrowse.ino' file.  By default, BOTH files will
+ * load when using the Sketchbook menu, and the .pde version will cause
+ * compiler errors in 1.0.  Delete the .pde, then load the sketch.
+ *
+ * I can't explain WHY this is necessary, but something among the various
+ * libraries here appears to be wreaking inexplicable havoc with the
+ * 'ARDUINO' definition, making the usual version test unusable (BOTH
+ * cases evaluate as true).  FML.
+ */
+
+/*
+ * This sketch uses the microSD card slot on the Arduino CC3000 shield
+ * to serve up files over a very minimal browsing interface
+ *
+ * Some code is from Bill Greiman's SdFatLib examples, some is from the
+ * Arduino Ethernet WebServer example, some from Limor Fried
+ * (Adafruit), and some from Tech500 so its probably under GPL
+ *
+ * Tutorial is at http://www.ladyada.net/learn/arduino/ethfiles.html
+ * Pull requests should go to http://github.com/adafruit/SDWebBrowse
+ */
+ 
+
+#include <SdFat.h>   
+#include <SdFatUtil.h>
+#include <Adafruit_CC3000.h>
+#include "utility/debug.h"   //https://github.com/adafruit/Adafruit_CC3000_Library
+#include <SPI.h>
+
+#define BUFSIZ 64  //Size of read buffer for file download  
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+										 
+// Local server IP, port
+uint32_t ip = cc3000.IP2U32(192,168,1,71);
+int port = 8889;									 
+										 
+#define WLAN_SSID       "Security"   // cannot be longer than 32 characters!
+#define WLAN_PASS       "09acdc7388"
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define LISTEN_PORT           8889      // What TCP port to listen on for connections.  
+                                      // The HTTP protocol uses port 80 by default.
+
+#define MAX_ACTION            10      // Maximum length of the HTTP action that can be parsed.
+
+#define MAX_PATH              64      // Maximum length of the HTTP request path that can be parsed.
+                                      // There isn't much memory available so keep this short!
+
+#define BUFFER_SIZE           MAX_ACTION + MAX_PATH + 20  // Size of buffer for incoming request data.
+                                                          // Since only the first line is parsed this
+                                                          // needs to be as large as the maximum action
+                                                          // and path plus a little for whitespace and
+                                                          // HTTP version.
+
+#define TIMEOUT_MS           500   // Amount of time in milliseconds to wait for
+                                     // an incoming request to finish.  Don't set this
+                                     // too high or your server could be slow to respond.
+
+Adafruit_CC3000_Server httpServer(LISTEN_PORT);
+uint8_t buffer[BUFFER_SIZE+1];
+int bufindex = 0;
+char action[MAX_ACTION+1];
+char path[MAX_PATH+1];
+
+/************ SDCARD STUFF ************/
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+SdFile file;
+
+// store error strings in flash to save RAM
+#define error(s) error_P(PSTR(s))
+
+void error_P(const char* str) {
+  PgmPrint("error: ");
+  SerialPrintln_P(str);
+  if (card.errorCode()) {
+    PgmPrint("SD error: ");
+    Serial.print(card.errorCode(), HEX);
+    Serial.print(',');
+    Serial.println(card.errorData(), HEX);
+  }
+  while(1);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  PgmPrint("Free RAM: ");
+  Serial.println(FreeRam());  
+
+  // initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
+  // breadboards.  use SPI_FULL_SPEED for better performance.
+  pinMode(53, OUTPUT);                       // set the SS pin as an output (necessary!)
+  digitalWrite(53, HIGH);                    // but turn off the W5100 chip!
+
+  if (!card.init(SPI_HALF_SPEED, 4)) error("card.init failed!");
+
+  // initialize a FAT volume
+  if (!volume.init(&card)) error("vol.init failed!");
+
+  PgmPrint("Volume is FAT");
+  Serial.println(volume.fatType(),DEC);
+  Serial.println();
+
+  if (!root.openRoot(&volume)) error("openRoot failed");
+
+  // list file in root with date and size
+  PgmPrintln("Files found in root:");
+  root.ls(LS_DATE | LS_SIZE);
+  Serial.println();
+
+  // Recursive list of all directories
+  PgmPrintln("Files found in all dirs:");
+  root.ls(LS_R);
+
+  Serial.println();
+  PgmPrintln("Done");
+
+  	Serial.println(F("Hello, CC3000!\n")); 
+
+	Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+
+	// Initialise the module
+	Serial.println(F("\nInitializing CC3000..."));
+	if (!cc3000.begin())
+	{
+	Serial.println(F("Couldn't begin()! Check your wiring?"));
+	while(1);
+	}
+  
+	Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
+	if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+	Serial.println(F("Failed!"));
+	while(1);
+	}
+
+	Serial.println(F("Connected!"));
+
+	Serial.println(F("Request DHCP"));
+	while (!cc3000.checkDHCP())
+	{
+	delay(100); // ToDo: Insert a DHCP timeout!
+	}  
+
+	// Display the IP address DNS, Gateway, etc.
+	while (! displayConnectionDetails()) {
+	delay(1000);
+	}
+
+	// Start listening for connections
+	httpServer.begin();
+  
+    Serial.println(F("Listening for connections..."));
+}
+
+char ListFiles(Adafruit_CC3000_ClientRef client, uint8_t flags, SdFile dir) {
+  // This code is just copied from SdFile.cpp in the SDFat library
+  // and tweaked to print to the client output in html!
+  dir_t p;
+
+  dir.rewind();
+  client.println("<ul>");
+  while (dir.readDir(&p) > 0) {
+    // done if past last used entry
+    if (p.name[0] == DIR_NAME_FREE) break;
+
+    // skip deleted entry and entries for . and  ..
+    if (p.name[0] == DIR_NAME_DELETED || p.name[0] == '.') continue;
+
+    // only list subdirectories and files
+    if (!DIR_IS_FILE_OR_SUBDIR(&p)) continue;
+
+    // print any indent spaces
+    client.print("<li><a href=\"");
+    for (uint8_t i = 0; i < 11; i++) {
+      if (p.name[i] == ' ') continue;
+      if (i == 8) {
+        client.print('.');
+      }
+      client.print((char)p.name[i]);
+    }
+    if (DIR_IS_SUBDIR(&p)) {
+      client.print('/');
+    }
+    client.print("\">");
+
+    // print file name with possible blank fill
+    for (uint8_t i = 0; i < 11; i++) {
+      if (p.name[i] == ' ') continue;
+      if (i == 8) {
+        client.print('.');
+      }
+      client.print((char)p.name[i]);
+    }
+    if (DIR_IS_SUBDIR(&p)) {
+      client.print('/');
+    }
+    client.print("</a>");
+
+
+
+    // print modify date/time if requested
+    if (flags & LS_DATE) {
+       dir.printFatDate(p.lastWriteDate);
+       client.print(' ');
+       dir.printFatTime(p.lastWriteTime);
+    }
+    // print size if requested
+    if (!DIR_IS_SUBDIR(&p) && (flags & LS_SIZE)) {
+      client.print(' ');
+      client.print(p.fileSize);
+    }
+    client.println("</li>");
+  }
+  client.println("</ul>");
+}
+
+// How big our line buffer should be. 100 is plenty!
+#define BUFFER 100
+
+
+void loop()
+{
+char clientline[BUFFER];
+  char name[17];
+  int index = 0;
+
+  Adafruit_CC3000_ClientRef client = httpServer.available();
+  if (client) {
+    // an http request ends with a blank line
+    boolean current_line_is_blank = true;
+
+    // reset the input buffer
+    index = 0;
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        // If it isn't a new line, add the character to the buffer
+        if (c != '\n' && c != '\r') {
+          clientline[index] = c;
+          index++;
+          // are we too big for the buffer? start tossing out data
+          if (index >= BUFFER) 
+            index = BUFFER -1;
+
+          // continue to read more data!
+          continue;
+        }
+
+        // got a \n or \r new line, which means the string is done
+        clientline[index] = 0;
+
+        // Print it out for debugging
+        Serial.println(clientline);
+
+        // Look for substring such as a request to get the root file
+        if (strstr(clientline, "GET / ") != 0) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+
+          // print all the files, use a helper to keep it clean
+          client.println("<h2>Files:</h2>");
+          ListFiles(client, LS_SIZE, root);
+        } else if (strstr(clientline, "GET /") != 0) {
+          // this time no space after the /, so a sub-file!
+          char *filename;
+
+          filename = clientline + 5; // look after the "GET /" (5 chars)
+          // a little trick, look for the " HTTP/1.1" string and 
+          // turn the first character of the substring into a 0 to clear it out.
+          (strstr(clientline, " HTTP"))[0] = 0;
+
+          // print the file we want
+          Serial.println(filename);
+
+          if (! file.open(&root, filename, O_READ)) {
+            client.println("HTTP/1.1 404 Not Found");
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("<h2>File Not Found!</h2>");
+            client.println("<br><h1>Couldn't open the File!</h3>");
+            break;
+          }
+
+          Serial.println("Opened!");
+
+          client.println("HTTP/1.1 200 OK");
+          if(file.isDir()) {
+            Serial.println("is directory");
+            //file.close();
+            client.println("Content-Type: text/html");
+            client.println();
+            client.print("<h2>Files in /");
+            file.getFilename(name);
+            client.print(name);
+            client.println("/:</h2>");
+            ListFiles(client,LS_SIZE,file);
+            file.close();
+          }   
+          else {
+
+            client.println("Content-Type: text/plain");
+            client.println();
+			
+			do   // @ adafruit_support_rick's do-while loop
+			{
+				int16_t c;
+				int count = 0;
+				char buffers[BUFSIZ];
+				bool done = false;
+				while ((!done) && (count < BUFSIZ) && (file.available()))
+				{
+				  char c = file.read();
+				  if (0 > c)
+					done = true;
+				  else
+					buffers[count++] = c;
+					delayMicroseconds(1000);
+				}
+				if (count)
+				client.write( buffers, count);
+				
+			} while (file.available());
+					
+			file.close();
+          }
+        } else {
+          // everything else is a 404
+          client.println("HTTP/1.1 404 Not Found");
+          client.println("Content-Type: text/html");
+          client.println();
+          client.println("<h2>File Not Found!</h2>");
+        }
+        break;
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    client.close();
+  }
+}  
+// Tries to read the IP address and other connection details
+///////////////////////////////////
+bool displayConnectionDetails(void)
+{
+	uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+	if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+	{
+		Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+		return false;
+	}
+	else
+	{
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+	}
+}
